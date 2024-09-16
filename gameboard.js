2ee674918e231239386a01b27526815d3b11b011
Trueman Phamdo <trueman@phamdo.com>
2024-09-16 16:12:53 -0700

    Update gameboard class to account for hits and misses

--- gameboard.js
+++ gameboard.js
@@ -1,5 +1,7 @@
 class Gameboard {
   #board;
+  #curShipId = 0;
+  #ships = [];
 
   constructor(size = 10) {
     this.#board = new Array(size);
@@ -7,30 +9,50 @@ class Gameboard {
     for (let i = 0; i < size; ++i) {
       this.#board[i] = new Array(size);
       for (let j = 0; j < size; ++j) {
-        this.#board[i][j] = -1;
+        this.#board[i][j] = "E";
       }
     }
   }
 
-  placeShip(startCoord, length, dir, id) {
+  placeShip(ship, startCoord, dir) {
     if (
-      !this.isPlacementValid(startCoord, length, dir) ||
-      !this.isPlacementEmpty(startCoord, length, dir)
+      !this.isPlacementValid(startCoord, ship.length, dir) ||
+      !this.isPlacementEmpty(startCoord, ship.length, dir)
     ) {
       throw new Error("Invalid placement");
     }
 
-    for (let i = 0; i < length; ++i) {
-      this.#board[startCoord.y][startCoord.x] = id;
+    for (let i = 0; i < ship.length; ++i) {
+      this.#board[startCoord.y][startCoord.x] = this.#ships.length;
       if (dir === "v") {
         startCoord.y++;
       } else {
         startCoord.x++;
       }
     }
+
+    this.#ships.push(ship);
+  }
+
+  receiveAttack(coord) {
+    if (!this.isCoordValid(coord)) return;
+    let idx = this.#board[coord.y][coord.x];
+    if (typeof idx === "number") {
+      this.#ships[idx].hit();
+      this.#board[coord.y][coord.x] = "H";
+    } else {
+      this.#board[coord.y][coord.x] = "M";
+    }
   }
 
   isPlacementValid(startCoord, length, dir) {
+    return (
+      this.isPlacementOnBoard(startCoord, length, dir) &&
+      this.isPlacementEmpty(startCoord, length, dir)
+    );
+  }
+
+  isPlacementOnBoard(startCoord, length, dir) {
     let destCoord = { x: startCoord.x, y: startCoord.y };
     if (dir === "v") {
       destCoord.y += length - 1;
@@ -41,12 +63,12 @@ class Gameboard {
   }
 
   isPlacementEmpty(startCoord, length, dir) {
-    if (!this.isPlacementValid(startCoord, length, dir)) return false;
+    if (!this.isPlacementOnBoard(startCoord, length, dir)) return false;
 
     let valid = true;
     let curCoord = { x: startCoord.x, y: startCoord.y };
     for (let i = 0; i < length; ++i) {
-      valid &= this.#board[curCoord.y][curCoord.x] === -1;
+      valid &= this.#board[curCoord.y][curCoord.x] === "E";
       if (dir === "v") {
         curCoord.y++;
       } else {
--- gameboard.test.js
+++ gameboard.test.js
@@ -1,12 +1,14 @@
 const Gameboard = require("./gameboard");
+const Ship = require("./ship");
 
 describe("board", function () {
   it("should be empty and default to size of 10", function () {
     const defaultLen = 10;
     let gb = new Gameboard();
+    expect(gb.size).toBe(defaultLen);
     for (let i = 0; i < defaultLen; ++i) {
       for (let j = 0; j < defaultLen; ++j) {
-        expect(gb.board[i][j]).toBe(-1);
+        expect(gb.board[i][j]).toBe("E");
       }
     }
   });
@@ -14,9 +16,10 @@ describe("board", function () {
   it("should be empty with custom size", function () {
     const len = 4;
     let gb = new Gameboard(len);
+    expect(gb.size).toBe(len);
     for (let i = 0; i < len; ++i) {
       for (let j = 0; j < len; ++j) {
-        expect(gb.board[i][j]).toBe(-1);
+        expect(gb.board[i][j]).toBe("E");
       }
     }
   });
@@ -27,60 +30,56 @@ describe("placeShip", function () {
     var testCases = [
       {
         coord: { x: 0, y: 0 },
-        length: 3,
+        ship: new Ship(3),
         dir: "h",
-        id: 0,
         size: 4,
         expected: [
-          [0, 0, 0, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
+          [0, 0, 0, "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
         ],
       },
       {
         coord: { x: 1, y: 1 },
-        length: 3,
+        ship: new Ship(3),
         dir: "v",
-        id: 0,
         size: 4,
         expected: [
-          [-1, -1, -1, -1],
-          [-1, 0, -1, -1],
-          [-1, 0, -1, -1],
-          [-1, 0, -1, -1],
+          ["E", "E", "E", "E"],
+          ["E", 0, "E", "E"],
+          ["E", 0, "E", "E"],
+          ["E", 0, "E", "E"],
         ],
       },
       {
         coord: { x: 0, y: 0 },
-        length: 4,
+        ship: new Ship(4),
         dir: "h",
-        id: 2,
         size: 4,
         expected: [
-          [2, 2, 2, 2],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
+          [0, 0, 0, 0],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
         ],
       },
       {
         coord: { x: 0, y: 0 },
-        length: 4,
+        ship: new Ship(4),
         dir: "v",
-        id: 2,
         size: 4,
         expected: [
-          [2, -1, -1, -1],
-          [2, -1, -1, -1],
-          [2, -1, -1, -1],
-          [2, -1, -1, -1],
+          [0, "E", "E", "E"],
+          [0, "E", "E", "E"],
+          [0, "E", "E", "E"],
+          [0, "E", "E", "E"],
         ],
       },
     ];
     testCases.forEach((tc) => {
       let gb = new Gameboard(tc.size);
-      gb.placeShip(tc.coord, tc.length, tc.dir, tc.id);
+      gb.placeShip(tc.ship, tc.coord, tc.dir);
 
       expect(gb.board).toEqual(tc.expected);
     });
@@ -89,16 +88,16 @@ describe("placeShip", function () {
   it("should throw error if invalid placement", function () {
     var testCases = [
       {
-        coord: { x: -1, y: 0 },
+        coord: { x: "E", y: 0 },
         length: 4,
         dir: "v",
         id: 0,
         size: 4,
         expected: [
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
         ],
       },
       {
@@ -108,10 +107,10 @@ describe("placeShip", function () {
         id: 0,
         size: 4,
         expected: [
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
         ],
       },
       ,
@@ -129,37 +128,37 @@ describe("placeShip", function () {
   it("Multiple ships: should update board with ships' id", function () {
     var testCases = [
       {
-        ships: [
-          { coord: { x: 0, y: 0 }, length: 3, dir: "h", id: 0 },
-          { coord: { x: 1, y: 1 }, length: 3, dir: "v", id: 1 },
+        placements: [
+          { coord: { x: 0, y: 0 }, ship: new Ship(3), dir: "h" },
+          { coord: { x: 1, y: 1 }, ship: new Ship(3), dir: "v" },
         ],
         size: 4,
         expected: [
-          [0, 0, 0, -1],
-          [-1, 1, -1, -1],
-          [-1, 1, -1, -1],
-          [-1, 1, -1, -1],
+          [0, 0, 0, "E"],
+          ["E", 1, "E", "E"],
+          ["E", 1, "E", "E"],
+          ["E", 1, "E", "E"],
         ],
       },
       {
-        ships: [
-          { coord: { x: 0, y: 0 }, length: 3, dir: "h", id: 0 },
-          { coord: { x: 0, y: 1 }, length: 3, dir: "h", id: 1 },
+        placements: [
+          { coord: { x: 0, y: 0 }, ship: new Ship(3), dir: "h" },
+          { coord: { x: 0, y: 1 }, ship: new Ship(3), dir: "h" },
         ],
         size: 4,
         expected: [
-          [0, 0, 0, -1],
-          [1, 1, 1, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
+          [0, 0, 0, "E"],
+          [1, 1, 1, "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
         ],
       },
     ];
 
     testCases.forEach((tc) => {
       let gb = new Gameboard(tc.size);
-      tc.ships.forEach((ship) => {
-        gb.placeShip(ship.coord, ship.length, ship.dir, ship.id);
+      tc.placements.forEach((p) => {
+        gb.placeShip(p.ship, p.coord, p.dir);
       });
       expect(gb.board).toEqual(tc.expected);
     });
@@ -168,28 +167,28 @@ describe("placeShip", function () {
   it("Multiple ships: should throw error if invalid placement", function () {
     var testCases = [
       {
-        ships: [
-          { coord: { x: 0, y: 0 }, length: 3, dir: "h", id: 0 },
-          { coord: { x: 1, y: 1 }, length: 4, dir: "v", id: 1 },
+        placements: [
+          { coord: { x: 0, y: 0 }, ship: new Ship(3), dir: "h" },
+          { coord: { x: 1, y: 1 }, ship: new Ship(4), dir: "v" },
         ],
         size: 4,
         expected: [
-          [0, 0, 0, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
+          [0, 0, 0, "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
         ],
       },
       {
-        ships: [
-          { coord: { x: 0, y: 3 }, length: 4, dir: "h", id: 0 },
-          { coord: { x: 0, y: 0 }, length: 4, dir: "v", id: 1 },
+        placements: [
+          { coord: { x: 0, y: 3 }, ship: new Ship(4), dir: "h" },
+          { coord: { x: 0, y: 0 }, ship: new Ship(4), dir: "v" },
         ],
         size: 4,
         expected: [
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
-          [-1, -1, -1, -1],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
           [0, 0, 0, 0],
         ],
       },
@@ -198,10 +197,104 @@ describe("placeShip", function () {
     testCases.forEach((tc) => {
       let gb = new Gameboard(tc.size);
       expect(() => {
-        tc.ships.forEach((ship) => {
-          gb.placeShip(ship.coord, ship.length, ship.dir, ship.id);
+        tc.placements.forEach((p) => {
+          gb.placeShip(p.ship, p.coord, p.dir);
         });
       }).toThrow("Invalid placement");
+      expect(gb.board).toEqual(tc.expected);
+    });
+  });
+});
+
+describe("receiveAttack", function () {
+  it("should update board with hit", function () {
+    const testCases = [
+      {
+        placements: [{ coord: { x: 0, y: 3 }, ship: new Ship(4), dir: "h" }],
+        size: 4,
+        expected: [
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          ["E", "E", "E", "E"],
+          [0, 0, "H", 0],
+        ],
+        attacks: [{ x: 2, y: 3 }],
+      },
+    ];
+
+    testCases.forEach((tc) => {
+      let gb = new Gameboard(tc.size);
+      tc.placements.forEach((p) => {
+        gb.placeShip(p.ship, p.coord, p.dir);
+      });
+      tc.attacks.forEach((a) => {
+        gb.receiveAttack(a);
+      });
+      expect(gb.board).toEqual(tc.expected);
+    });
+  });
+
+  it("should update board with miss", function () {
+    const testCases = [
+      {
+        placements: [{ coord: { x: 0, y: 3 }, ship: new Ship(4), dir: "h" }],
+        size: 4,
+        expected: [
+          ["E", "E", "E", "E"],
+          ["E", "M", "E", "E"],
+          ["E", "E", "M", "E"],
+          [0, 0, 0, 0],
+        ],
+        attacks: [
+          { x: 2, y: 2 },
+          { x: 1, y: 1 },
+        ],
+      },
+    ];
+
+    testCases.forEach((tc) => {
+      let gb = new Gameboard(tc.size);
+      tc.placements.forEach((p) => {
+        gb.placeShip(p.ship, p.coord, p.dir);
+      });
+      tc.attacks.forEach((a) => {
+        gb.receiveAttack(a);
+      });
+      expect(gb.board).toEqual(tc.expected);
+    });
+  });
+
+  it("should update board with multiple misses and hits", function () {
+    const testCases = [
+      {
+        placements: [
+          { coord: { x: 0, y: 3 }, ship: new Ship(4), dir: "h" },
+          { coord: { x: 2, y: 0 }, ship: new Ship(3), dir: "v" },
+        ],
+        size: 4,
+        expected: [
+          ["E", "E", 1, "E"],
+          ["E", "M", 1, "E"],
+          ["M", "E", "H", "E"],
+          [0, 0, 0, "H"],
+        ],
+        attacks: [
+          { x: 0, y: 2 },
+          { x: 1, y: 1 },
+          { x: 2, y: 2 },
+          { x: 3, y: 3 },
+        ],
+      },
+    ];
+
+    testCases.forEach((tc) => {
+      let gb = new Gameboard(tc.size);
+      tc.placements.forEach((p) => {
+        gb.placeShip(p.ship, p.coord, p.dir);
+      });
+      tc.attacks.forEach((a) => {
+        gb.receiveAttack(a);
+      });
       expect(gb.board).toEqual(tc.expected);
     });
   });
