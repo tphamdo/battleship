62f3b07a133cbca7937ae7ca9a2ae991e299d700
Trueman Phamdo <trueman@phamdo.com>
2024-09-19 11:23:53 -0400

    Add prod and dev webpack configs + Add prettier dependency and deploy script

--- webpack.common.js
+++ webpack.common.js
@@ -3,13 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
